3b57468524e4a16226dca5fd3bfeea4eb098d2c3
David Warde-Farley <dwf@cs.toronto.edu>
2009-05-04 18:13:38 -0400

    Added support for evaluating likelihood of test data.

--- csv2labels.py
+++ csv2labels.py
@@ -1,8 +1,10 @@
-import csv, sys
+import csv, sys, re
+import numpy as np
+import pdb
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print >> sys.stderr, "%s <geneDBfile> <wellDBfile>" % sys.argv[0]
+    if len(sys.argv) != 5:
+        print >> sys.stderr, "%s <geneDBfile> <wellDBfile> ids.npz all_cell_data.npz" % sys.argv[0]
         sys.exit(1)
     
     orfs_to_labels = {}
@@ -23,6 +25,48 @@ if __name__ == "__main__":
             loc = tuple(map(int, (line['nplate'],line['nrow'],line['ncol'])))
             locs_to_orfs[loc] = line['cgeneorf']
     
-    # If a gene has more than one associated label.
+    locs_to_labels = {}
+    for loc in locs_to_orfs:
+        orf = locs_to_orfs[loc]
+        try:
+            rec = orfs_to_labels[orf]
+        except KeyError:
+            print >>sys.stderr, "[w] Gene '%s' not in label lookup." % orf
+            continue
+        localization = None
+        for key, val in rec.iteritems():
+            if key[:5] == "bloc_":
+                if localization == None and val == "1":
+                    localization = key
+                elif val == "1":
+                    print >> sys.stderr, "[w] '%s': multiple loc'ns" % orf
+                    continue
+        if localization:
+            locs_to_labels[loc] = localization
+        else:
+            print >> sys.stderr, "[w] '%s': no localization" % orf
+    f = np.load(sys.argv[3])
+    platerowcol2imageno = {}
+    for fn in f.files:
+        tokens = fn.split("_")
+        print tokens
+        plateno = re.match(r'plate(\d\d)',tokens[-3]).group(1)
+        row, col = re.match(r'(\d{3})(\d{3})(\d{3})',tokens[-2]).groups()[:2]
+        imageno = f[fn][0,0]
+        platerowcol2imageno[(int(plateno),int(row),int(col))] = imageno
+    #print platerowcol2imageno
+    g = np.load(sys.argv[4])
     
-
+    labels = np.empty(g['order'].shape,dtype=object)
+    imageno2label = {}
+    for loc,label in locs_to_labels.iteritems():
+        try:
+            imageno2label[platerowcol2imageno[loc]] = label
+        except KeyError, e:
+            pass
+    
+    labeled_images = np.unique1d(np.array(imageno2label.keys()))
+    for ii in labeled_images:
+        labels[g['ids'][:,0] == ii] = imageno2label[ii]d
+    print "Saving..."
+    np.save("/Users/dwf/Data/labels.npz", labels)
