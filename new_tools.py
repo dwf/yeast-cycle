76ff41de9bcab9de8b9ce0e6d92002d1e500a86c
David Warde-Farley <dwf@cs.toronto.edu>
2009-05-29 18:58:39 -0400

    Chaco test

--- new_tools.py
+++ new_tools.py
@@ -1,9 +1,8 @@
-from itertools import izip, islice
+"""Traits-based tools for image preprocessing."""
+from itertools import islice
 
 # For our matplotlib figure
-import matplotlib
 from matplotlib.figure import Figure
-import pdb
 
 # For image operations and file I/O, ndimage and pytables
 import tables
@@ -11,19 +10,26 @@ import numpy as np
 import scipy.ndimage as ndimage
 
 # Traits-related imports
-from enthought.traits.api import HasTraits, Instance, Int, Array, List, Str, Property, Tuple
+from enthought.traits.api import Int, Str
+from enthought.traits.api import List, Tuple, Array
+from enthought.traits.api import HasTraits, Instance, Property
+
+# Chaco
+from enthought.chaco.api import ArrayPlotData, Plot, bone, jet
+from enthought.enable.component_editor import ComponentEditor
 
 # View components
-from enthought.traits.ui.api import View, Item, Group, Handler, VGroup
+from enthought.traits.ui.api import View, Item, Group, VGroup
 
 # Editor components
-from enthought.traits.ui.api import RangeEditor, InstanceEditor
+from enthought.traits.ui.api import RangeEditor
 from embedded_figure import MPLFigureEditor
 
 from tools import fit_ellipse, spline_features, radians_to_degrees
 from tools import ObjectTooSmallError
 
 class ObjectSilhouette(HasTraits):
+    """Class representing a single cell silhouette in an image."""
     figure = Instance(Figure, ())    
     image = Array(dtype=bool) 
     
@@ -33,54 +39,52 @@ class ObjectSilhouette(HasTraits):
         self.image = image
     
     def process_item(self, fig):
-        im = self.image
+        image = self.image
         # Fit an ellipse using every nonzero pixel location in the image.
-        coeffs = fit_ellipse(*(np.where(im)))
+        coeffs = fit_ellipse(*(np.where(image)))
         if coeffs.size != 6:
-            print >>sys.stderr, "[e] Ellipse fit im #%d, obj #%d" % \
-                tuple(ids[ii])
+            print "[e] Ellipse fit im #%d, obj #%d"
             return
         else:
-            a,b,c,d,e,f = coeffs
-        preangle = b / (a - c)
+            coeff_a, coeff_b, coeff_c = coeffs[:3]
+        preangle = coeff_b / (coeff_a - coeff_c)
         if not np.isinf(preangle):
             angle = radians_to_degrees(-0.5 * np.arctan(preangle))
-            old_angle = angle
             # Order = 0 prevents interpolation from being done and screwing 
             # with our object boundaries.
-            rotated = ndimage.rotate(im, angle, order=0)
+            rotated = ndimage.rotate(image, angle, order=0)
             height, width = rotated[ndimage.find_objects(rotated)[0]].shape
         else:
             angle = 0.
-            old_angle = 0.
+            
         if width > height:
             angle -= 90.0
-            rotated = ndimage.rotate(im, angle, order=0)
+            rotated = ndimage.rotate(image, angle, order=0)
         # Correct so that in budding cells, the "major" hump is always
         # on the first.          
         if np.argmax(rotated.sum(axis=1)) > rotated.shape[0] // 2:
             angle -= 180.0
-            rotated = ndimage.rotate(im, angle, order=0)
+            rotated = ndimage.rotate(image, angle, order=0)
         
         # Do a find_objects on the resultant array after rotation in 
         # order to _just_ get the object and not any of the extra 
         # space that's been added.
         try:
             bounds = ndimage.find_objects(rotated)[0]
-        except IndexError, e:
+        except IndexError:
             pass
         
         try:
-            these_features = spline_features(rotated[bounds],plot=True,
+            spline_features(rotated[bounds], plot=True,
                     fig=fig)
-        except ObjectTooSmallError, e:
+        except ObjectTooSmallError:
             pass
     
 
 class ImageSilhouette(HasTraits):
     """Class representing a silhouette image of segmented cells."""
     label_image = Array()
-    object_slices = List(Tuple(slice,slice), ())
+    object_slices = List(Tuple(slice, slice), ())
     
     def __init__(self, node):
         super(ImageSilhouette, self).__init__()
@@ -99,8 +103,8 @@ class ImageSilhouette(HasTraits):
             indices = islice(xrange(len(self)), *key.indices(len(self)))
             return [self[nkey] for nkey in indices]
         else:
-            im = self.label_image[self.object_slices[key]] == (key + 1)
-            return ObjectSilhouette(im)
+            image = self.label_image[self.object_slices[key]] == (key + 1)
+            return ObjectSilhouette(image)
     
     def __contains__(self):
         raise TypeError("Containment checking not supported")
@@ -126,8 +130,8 @@ class Plate(HasTraits):
                 *key.indices(len(self.images)))
             return [self[nkey] for nkey in indices]
         else:
-            im = self.node._v_file.getNode(self.images[key])
-            return ImageSilhouette(im)
+            image = self.node._v_file.getNode(self.images[key])
+            return ImageSilhouette(image)
 
     def __contains__(self):
         raise TypeError("Containment checking not supported")
@@ -171,12 +175,15 @@ class DataSetBrowser(HasTraits):
     to navigate through plates, images within plates, and objects 
     within images.
     """
+    # Chaco plot
+    plot = Instance(Plot)
+    
     # matplotlib Figure instance
     figure = Instance(Figure, ())
-
+    
     # DataSet being viewed
     dataset = Instance(DataSet)
-
+    
     # Plate object currently being examined
     current_plate = Instance(Plate)
 
@@ -198,17 +205,17 @@ class DataSetBrowser(HasTraits):
     num_plates = Property(Int, depends_on='dataset')
     num_images = Property(Int, depends_on='current_plate')
     num_objects = Property(Int, depends_on='current_image')
-
+    
     view = View(
-        VGroup(
-            Item('figure', editor=MPLFigureEditor(), show_label=False), 
-            Group(Item('object_index'),
-                Item('image_index'),
-                Item('plate_index'))
-        ),
-        height=600,
-        width=700,
-        resizable=True)
+            VGroup(
+                Item('figure', editor=MPLFigureEditor(), show_label=False), 
+                Group(Item('object_index'),
+                    Item('image_index'),
+                    Item('plate_index'))
+            ),
+            height=600,
+            width=700,
+            resizable=True)
 
     def __init__(self, dataset, **metadata):
         """Construct a DataSetBrowser from the specified DataSet object."""
@@ -221,7 +228,14 @@ class DataSetBrowser(HasTraits):
         self.figure.add_subplot(211)
         self.figure.add_subplot(212)
         self._object_index_changed()
-
+        
+        plotdata = ArrayPlotData(imagedata=self.current_object.image)
+        plot = Plot(plotdata)
+        xbounds = np.arange(self.current_object.image.shape[1])
+        ybounds = np.arange(self.current_object.image.shape[0])
+        plot.img_plot("imagedata", xbounds=xbounds, ybounds=ybounds,colormap=bone)
+        self.plot = plot
+        
     ######################### Private interface ##########################    
 
     def _plate_index_changed(self):
